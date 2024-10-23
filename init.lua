bdad701b2d2152f4f4298755096e7114f21377e4
ProfessionalMenace <vondracekadam00@gmail.com>
2024-10-24 01:38:40 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,9 +6,11 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
+vim.o.number = true
 vim.o.wrap = false
 -- Colorscheme
 vim.cmd.colorscheme("nightfox")
+-- LSP
 lspconfig = require("lspconfig")
 lspconfig.clangd.setup{}
 lspconfig.rust_analyzer.setup{}
