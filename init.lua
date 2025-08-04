4f8c6d06c3a01f0dcc939c17254a19d1cc272d01
ProfessionalMenace <vondracekadam00@gmail.com>
2025-08-05 00:35:12 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,12 +1,18 @@
 -- Lazy.nvim
-require("config.lazy")
+require('config.lazy')
 -- Tabs
 vim.o.tabstop = 4
-vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.smarttab = true
 vim.o.smartindent = true
+
+-- Configuration
 vim.o.number = true
+vim.o.relativenumber = true
 vim.o.linebreak = true
+vim.g.mapleader = ' '
+vim.o.winborder = 'rounded'
+
 -- Colorscheme
-vim.cmd.colorscheme("nightfox")
+vim.cmd.colorscheme('nightfox')
