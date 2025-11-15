fcef6813fc5b19173a00903b5ac0bd65e9dd11d2
Menace <vondracekadam00@gmail.com>
2025-11-15 02:37:26 +0100

    lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,29 +1,27 @@
-return {
- {
-    'neovim/nvim-lspconfig',
-    config = function()
-      -- CLANGD
-      vim.lsp.enable('clangd')
-      vim.lsp.config('clangd', {})
-     
-      -- RUST-ANALYZER
-      vim.lsp.enable('rust_analyzer')
-      vim.lsp.config('rust_analyzer', {
-        settings = {
-          ['rust-analyzer'] = {
-            checkOnSave = {
-            command = "clippy",
-              extraArgs = {"--workspace"},
-            },
-            diagnostics = {
-                enable = true,
-            },
-            procMacro = {
-              enable = true,
-            },
+return {{
+  'neovim/nvim-lspconfig',
+  config = function()
+    -- CLANGD
+    vim.lsp.enable('clangd')
+    vim.lsp.config('clangd', {})
+   
+    -- RUST-ANALYZER
+    vim.lsp.enable('rust_analyzer')
+    vim.lsp.config('rust_analyzer', {
+      settings = {
+        ['rust-analyzer'] = {
+          checkOnSave = {
+          command = "clippy",
+            extraArgs = {"--workspace"},
           },
-        }
-      })
-     end,
-  }
-}
+          diagnostics = {
+              enable = true,
+          },
+          procMacro = {
+            enable = true,
+          },
+        },
+      }
+    })
+   end,
+}}
