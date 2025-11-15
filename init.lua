6b58380459b1717ab3a6b940117b238314b955df
Menace <vondracekadam00@gmail.com>
2025-11-15 02:34:59 +0100

    autocomplete

--- init.lua
+++ init.lua
@@ -28,23 +28,8 @@ vim.keymap.set('v', '<leader>y', '"+y<CR>')
 -- Colorscheme
 vim.cmd.colorscheme('nightfox')
 
--- TMP
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-      vim.keymap.set('i', '<C-Space>', function()
-        vim.lsp.completion.get()
-      end)
-    end
-  end,
-})
+-- Spell 
+vim.opt.spelllang = en_us;
 
--- Diagnostics
-vim.diagnostic.config({
-  virtual_lines = {
-    current_line = true,
-  },
-})
+-- Autocompleteopt
+vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
